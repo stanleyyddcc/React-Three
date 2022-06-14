782543856566f91c7aa489d7f8c1ac68ca781a06
stanleyyddcc <stanleyyddcc@gmail.com>
2022-06-14 22:18:20 +0700

    React Three Project

---
+++ my-app/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Main from "./Main";
+class App extends React.Component{
+    render(){
+        return(
+            <div>
+                <Main />
+            </div>
+        );
+    }
+}
+
+export default App;
